5d29000a92a5d7166f96616b2ef1d3f1beca8efb
chrischris292 <cbchan2@illinois.edu>
2014-12-02 11:20:47 -0600

    sentiment package

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var server = require('http').createServer(app);
 var io = require('../..')(server);
 var port = process.env.PORT || 3000;
 var scraperjs = require('scraperjs');
+var analyze = require('Sentimental').analyze,
+    positivity = require('Sentimental').positivity,
+    negativity = require('Sentimental').negativity;
+
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
